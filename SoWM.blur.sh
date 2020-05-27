c3721cc5b11f1e73f37725af66f320839e3d26c8
ssfgames13 <ssfgames13@gmail.com>
2020-05-27 12:53:16 -0400

    Added SoWM.blur.sh > Blur For sowm

---
+++ SoWM.blur.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wp="///home/thatgeekyweeb/Downloads/bqjps3cdq7t41_illustration_x4_colored_toned.png"
+wp_blurred="/home/thatgeekyweeb/bin/blur.png"
+while sleep 1
+ do if [ "$(lsw)" ]
+      then feh --bg-fill --no-fehbg "$wp_blurred"
+      else feh --bg-fill --no-fehbg "$wp"
+    fi
+done
